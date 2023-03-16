3d8cef3eb13fac00d341a6dffb961a2fd68f82d4
jooleer <60648388+jooleer@users.noreply.github.com>
2023-03-17 00:26:28 +0700

    initial commit

---
+++ CS50P/week7/test_numb3rs.py
@@ -0,0 +1,8 @@
+from numb3rs import validate
+
+
+def test_validate():
+    assert validate("127.0.0.1") == True
+    assert validate("255.255.255.255") == True
+    assert validate("10.10.10") == False
+    assert validate("10.10.100.300") == False

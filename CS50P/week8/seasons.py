76fe9cb33f738b3aa0d9dab1ee22dab251dd7d44
jooleer <60648388+jooleer@users.noreply.github.com>
2023-03-30 21:01:20 +0700

    Update seasons.py
    working version

--- CS50P/week8/seasons.py
+++ CS50P/week8/seasons.py
@@ -1,23 +1,37 @@
-from datetime import date
+import datetime
+import inflect
 import re
 import sys
 
+# set p as variable for inflect engine
+p = inflect.engine()
+
 
 def main():
-    dob = input("What's your birth date? ")
-    date_format = r"([1-2][0-9][0-9][0-9])-([0-1][0-9])-([0-3][0-9])"
-    check = re.search(date_format, dob)
-
-    if check:
-        convert(dob)
-    else:
-        sys.exit("Invalid date")
+    print(convert(input("Date of Birth: ")))
 
 
 def convert(dob):
-    year, month, day = dob.split("-")
+    # check if date follows yyyy-mm-dd format
+    date_format = r"([1-2][0-9][0-9][0-9])-([0-1][0-9])-([0-3][0-9])"
+    check = re.search(date_format, dob)
+
+    # exit if date is not correctly formatted
+    if not check:
+        sys.exit("Invalid date")
+
+    # subtract dob with today's date and convert the result to minutes
     daily = 1440
-    
+    today = datetime.date.today()
+    dob = datetime.datetime.strptime(dob, "%Y-%m-%d").date()
+    days = today - dob
+    minutes = daily * days.days
+
+    # convert minutes number to words, capitalize and complete string
+    words = p.number_to_words(minutes, andword="")
+    output = words.capitalize() + " minutes"
+
+    return (output)
 
 
 if __name__ == "__main__":

4e1a1021281b849d91ba2e3f6bc2d9c7f4e18ffd
jooleer <60648388+jooleer@users.noreply.github.com>
2023-03-30 21:01:32 +0700

    Update test_seasons.py

--- CS50P/week8/test_seasons.py
+++ CS50P/week8/test_seasons.py
@@ -1,5 +1,11 @@
 from seasons import convert
+import pytest
 
 
 def test_convert():
-    # todo
+    assert convert(
+        "1990-10-10") == "Seventeen million, seventy-six thousand, nine hundred sixty minutes"
+    assert convert(
+        "2000-01-01") == "Twelve million, two hundred twenty-four thousand, one hundred sixty minutes"
+    with pytest.raises(SystemExit):
+        assert convert("bark bark") == SystemExit
